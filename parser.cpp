ff6461cd7333ad5222be2d2c30d2fa54773838a8
Marcel Greter <marcel.greter@ocbnet.ch>
2015-06-13 17:54:45 +0200

    Fix parsing edge case with numbers and trailing escapes
    Fixes https://github.com/sass/libsass/issues/1219

--- parser.cpp
+++ parser.cpp
@@ -1384,8 +1384,7 @@ namespace Sass {
     if (lex< kwd_important >())
     { return new (ctx.mem) String_Quoted(pstate, "!important"); }
 
-    const char* stop;
-    if ((stop = peek< value_schema >()))
+    if (const char* stop = peek< value_schema >())
     { return parse_value_schema(stop); }
 
     // string may be interpolated
